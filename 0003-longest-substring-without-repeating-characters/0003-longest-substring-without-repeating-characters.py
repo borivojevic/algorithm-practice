0f18867332dc63afacd810448dcbe649acd5058a
Mirko Borivojevic <mirko@mirkoborivojevic.com>
2023-11-27 21:02:14 +0100

    Update 0003-longest-substring-without-repeating-characters.py

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,32 +1,13 @@
 class Solution(object):
     def lengthOfLongestSubstring(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-
-        # base case
-        if (len(s) <= 1):
-            return len(s)
-        
-        # find longest sequence
-        longest_sequence = 0
-        outer_index = 0
-
-        while outer_index < len(s):
-            index = outer_index
-            sequence = ""
-            while index < len(s) and s[index] not in sequence:
-                sequence += s[index]
-                index += 1
-
-            longest_sequence = max(len(sequence), longest_sequence)
-
-            # base case
-            if index == len(s):    
-                outer_index = len(s)
+        seen = {}
+        l = 0
+        length = 0
+        for r in range(len(s)):
+            char = s[r]
+            if char in seen and seen[char] >= l:
+                l = seen[char] + 1
             else:
-                outer_index += 1
-        
-        return longest_sequence
-        
+                length = max(length, r - l + 1)
+            seen[char] = r
+        return length

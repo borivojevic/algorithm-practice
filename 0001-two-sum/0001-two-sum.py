aaa39db18a2b14ddded9213ce515118961972c5e
Mirko Borivojevic <mirko@mirkoborivojevic.com>
2023-12-01 17:30:06 +0100

    Time: 34 ms (90.07%), Space: 14.2 MB (24.14%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        seen = {}
+        for i in range(len(nums)):
+            diff = target - nums[i]
+            if diff in seen:
+                return [i, seen[diff]]
+            else:
+                seen[nums[i]] = i
